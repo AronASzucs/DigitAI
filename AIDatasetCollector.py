7047a10e3f36aceb77cb44822817fef0e3ee816f
Mrguccu <mrguccu@gmail.com>
2025-03-20 18:32:34 -0500

    Added checks for directories. Makes new directories now if needed. change px buttons now

--- AIDatasetCollector.py
+++ AIDatasetCollector.py
@@ -1,4 +1,3 @@
-import time
 import tkinter as tk
 import tkinter.font as tkFont
 import numpy
@@ -11,7 +10,6 @@ class AIDatasetCollector:
 
     def __init__(self, root):
 
-        # Dark Color Palette
         # Dark Color Palette
         self.colorhighlight = "#8cbfc2"
         self.highlight = "#FFFFFF"
@@ -51,7 +49,7 @@ class AIDatasetCollector:
         windll.shcore.SetProcessDpiAwareness(1)
         self.root.geometry("512x512")
         self.root.title("Handwritten Number AI collection and model")
-        #self.root.resizable(False, False)
+        self.root.resizable(False, False)
         self.root.iconbitmap("assets/Icon.ico")
         root.configure(bg = "#292929")
 
@@ -61,7 +59,7 @@ class AIDatasetCollector:
         self.helpLabel.pack()
 
         # Configure Columns
-        self.inputButtomFrame = tk.Frame(self.root)
+        self.inputButtomFrame = tk.Frame(self.root, background=self.altbackground)
         for i in range(7):
             self.inputButtomFrame.columnconfigure(i, weight=1)
 
@@ -73,19 +71,21 @@ class AIDatasetCollector:
             button.grid(row=i // 5, column=i % 5)
             self.buttons[i] = button
 
-        Button1 = tk.Button(self.inputButtomFrame, text = "8px", font=("Arial", self.fontSize),background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
-        Button2 = tk.Button(self.inputButtomFrame, text = "16px",font=("Arial", self.fontSize), background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
-        Button3 = tk.Button(self.inputButtomFrame, text = "28px", font=("Arial", self.fontSize),background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
-        Button4 = tk.Button(self.inputButtomFrame, text = "32px",font=("Arial", self.fontSize), background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+        self.button_8px = tk.Button(self.inputButtomFrame, text = "8px", font=("Arial", self.fontSize), command=lambda: self.change_px_button_color(8), background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+        self.button_8px.grid(row=0, column=6)
 
-        Button1.grid(row=0, column=5)
-        Button2.grid(row=0, column=6)
-        Button3.grid(row=1, column=5)
-        Button4.grid(row=1, column=6)
+        self.button_16px = tk.Button(self.inputButtomFrame, text = "16px", font=("Arial", self.fontSize), command=lambda: self.change_px_button_color(16), background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+        self.button_16px.grid(row=0, column=7)
 
+        self.button_32px = tk.Button(self.inputButtomFrame, text = "32px", font=("Arial", self.fontSize), command=lambda: self.change_px_button_color(32),background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+        self.button_32px.grid(row=1, column=6)
 
+        self.button_64px = tk.Button(self.inputButtomFrame, text = "64px", font=("Arial", self.fontSize), command=lambda: self.change_px_button_color(64),background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+        self.button_64px.grid(row=1, column=7)
+            
         self.inputButtomFrame.pack()
         self.change_number_button_color(0) #sets the 0 to be highlighted
+        self.change_px_button_color(16) # sets 16 to be highlighted
 
         self.numInDirectoryButton = tk.Label(self.root, text="Images in directory: " + str(self.image_count), font=("Arial", 10), foreground="White", background=self.background)
         self.numInDirectoryButton.pack()
@@ -115,17 +115,36 @@ class AIDatasetCollector:
 
         self.buttons[num].config(background = self.colorhighlight)
 
+    def change_px_button_color(self,num):
+        self.button_8px.config(background=self.altbackground)
+        self.button_16px.config(background=self.altbackground)
+        self.button_32px.config(background=self.altbackground)
+        self.button_64px.config(background=self.altbackground)
+        self.imageDimensions = num
+        self.array = numpy.zeros((self.imageDimensions, self.imageDimensions))
+
+        print(self.imageDimensions)
+
+        if num == 8:
+            self.button_8px.config(background=self.colorhighlight)
+        elif num == 16:
+            self.button_16px.config(background=self.colorhighlight)
+        elif num == 32:
+            self.button_32px.config(background=self.colorhighlight)
+        elif num == 64:
+            self.button_64px.config(background=self.colorhighlight)
+
     def changeColorPalette(self):
 
         if self.LightOrDark == 0:
             self.trainModelButton.configure(background=self.light_altbackground, highlightcolor=self.light_highlight)
             self.root.config(background=self.light_background)
-            print("now light")
+            print("now light mode")
             self.LightOrDark = 1
         else:
             self.trainModelButton.configure(background=self.altbackground)
             self.root.config(background=self.background)
-            print("now dark")
+            print("now dark mode")
             self.LightOrDark = 0
 
     def mouseDrag(self, event):
@@ -134,24 +153,44 @@ class AIDatasetCollector:
         print (str(x // (256 // self.imageDimensions)) + " " + str(y // (256 // self.imageDimensions)))
 
     def reset(self, event):
-        time.sleep(0.1)
         self.drawingCanvas.delete("all")
         self.array = numpy.zeros((self.imageDimensions, self.imageDimensions))
 
     def saveImg(self, event):
+        #draws image from array
         image = Image.fromarray((self.array * 255).astype(numpy.uint8))
 
-        file_path = "dataset/" + str(self.currentNum) + "/num" + str(self.currentNum) + "count" + str(self.image_count) + ".png"
+        file_path = "dataset/" + str(self.imageDimensions) + "px/" + str(self.currentNum) + "/num" + str(self.currentNum) + "count" + str(self.image_count + 1) + ".png"
+
+        # makes dataset/ directory if not found
+        if not os.path.isdir("dataset/"):
+            os.mkdir("dataset")
+            print("made directory dataset/")
+
+        # makes px directory if not found
+        if not os.path.isdir("dataset/" + str(self.imageDimensions) + "px/"):
+            os.mkdir("dataset/" + str(self.imageDimensions) + "px/")
+            print("made directory dataset/" + str(self.imageDimensions) + "px/")
+
+        #makes imageDim directory if not found
+        if not os.path.isdir("dataset/" + str(self.imageDimensions) + "px/" + str(self.currentNum) + "/"):
+            os.mkdir("dataset/" + str(self.imageDimensions) + "px/" + str(self.currentNum) + "/")
+            print("made directory dataset/" + str(self.imageDimensions) + "px/" + str(self.currentNum) + "/")
+
         image.save(file_path)
 
         self.reset(self)
 
     def updateImagesInDirectory(self):
-        file_path = "dataset/" + str(self.currentNum) + "/"
+        file_path = "dataset/" + str(self.imageDimensions) + "px/"+ str(self.currentNum) + "/"
 
-        # finds the number of files in a given directory
-        # found on stack ovecanvasrflow
-        self.image_count = len(os.listdir(file_path))
+        # unable to find path
+        if not os.path.isdir(file_path):
+            self.image_count = 0
+        else:
+            # finds the number of files in a given directory
+            # found on stack ovecanvasrflow
+            self.image_count = len(os.listdir(file_path))
 
         self.numInDirectoryButton.config(text="Images in directory: " + str(self.image_count))
 
