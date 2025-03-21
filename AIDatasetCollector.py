00eb81e49c38ae14224e29586baaf17ac747b6b6
Mrguccu <mrguccu@gmail.com>
2025-03-21 11:12:56 -0500

    Changed from camelCase to snake_case

--- AIDatasetCollector.py
+++ AIDatasetCollector.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import tkinter.font as tkFont
 import numpy
 from PIL import Image
 import os, os.path
@@ -11,156 +10,161 @@ class AIDatasetCollector:
     def __init__(self, root):
 
         # Dark Color Palette
-        self.colorhighlight = "#8cbfc2"
+        self.color_highlight = "#8cbfc2"
         self.highlight = "#FFFFFF"
         self.background = "#292929"
-        self.altbackground = "#383838"
-        self.activebackground = "#5e5e5e"
+        self.alt_background = "#383838"
+        self.active_background = "#5e5e5e"
         self.canvas_color = "#1f1f1f"
 
         # Light Color Palette
-        self.light_colorhighlight = "Black"
+        self.light_color_highlight = "Black"
         self.light_highlight = "Black"
         self.light_background = "White"
-        self.light_altbackground = "Gray"
-        self.light_activebackground = "Light Gray"
+        self.light_alt_background = "Gray"
+        self.light_active_background = "Light Gray"
         self.light_canvas_color = "White"
 
-        self.LightOrDark = 0 # 0 = dark mode, 1 = light mode
+        self.light_or_dark = 0 # 0 = dark mode, 1 = light mode
 
-        self.imageDimensions = 16
+        self.image_dimensions = 16
         self.root = root
-        self.fontSize = 18
-        self.currentNum = 0
+        self.font_size = 18
+        self.current_num = 0
         self.image_count = 0
         
-        self.setupWindow()
-        self.createWidgets()
-        self.updateImagesInDirectory()
+        self.setup_window()
+        self.create_widgets()
+        self.update_images_in_directory()
 
         self.root.bind("<r>", self.reset)
-        self.root.bind("<s>", self.saveImg)
-        self.drawingCanvas.bind("<B1-Motion>", self.mouseDrag)
+        self.root.bind("<s>", self.save_img)
+        self.drawing_canvas.bind("<B1-Motion>", self.mouse_drag)
 
         # define array
-        self.array = numpy.zeros((self.imageDimensions, self.imageDimensions))
+        self.array = numpy.zeros((self.image_dimensions, self.image_dimensions))
 
-    def setupWindow(self):
+    def setup_window(self):
         windll.shcore.SetProcessDpiAwareness(1)
-        self.root.geometry("512x512")
+        self.root.geometry("600x600")
         self.root.title("Handwritten Number AI collection and model")
-        self.root.resizable(False, False)
+        #self.root.resizable(False, False)
         self.root.iconbitmap("assets/Icon.ico")
         root.configure(bg = "#292929")
 
-    def createWidgets(self):
+    def create_widgets(self):
         # Help Info Label
-        self.helpLabel = tk.Label(self.root, text="r = Reset, s = Save", font=tkFont.Font(family="assets/Lexend.ttf", size=20), foreground=self.highlight, background=self.background)
-        self.helpLabel.pack()
+        self.help_label = tk.Label(self.root, text="r = Reset, s = Save", font=("System", self.font_size), foreground=self.highlight, background=self.background)
+        self.help_label.pack()
 
         # Configure Columns
-        self.inputButtomFrame = tk.Frame(self.root, background=self.altbackground)
+        self.input_buttom_frame = tk.Frame(self.root, background=self.alt_background)
         for i in range(7):
-            self.inputButtomFrame.columnconfigure(i, weight=1)
+            self.input_buttom_frame.columnconfigure(i, weight=1)
 
         # Create Buttons
         self.buttons = {}
 
         for i in range(10):
-            button = tk.Button(self.inputButtomFrame, text=str(i), font=("Arial", self.fontSize), command=lambda t=i: self.change_number_button_color(t), background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+            button = tk.Button(self.input_buttom_frame, text=str(i), font=("System", self.font_size), command=lambda t=i: self.change_number_button_color(t), background=self.alt_background, borderwidth=0, activebackground=self.active_background, foreground=self.highlight, activeforeground=self.highlight)
             button.grid(row=i // 5, column=i % 5)
             self.buttons[i] = button
 
-        self.button_8px = tk.Button(self.inputButtomFrame, text = "8px", font=("Arial", self.fontSize), command=lambda: self.change_px_button_color(8), background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+        self.button_8px = tk.Button(self.input_buttom_frame, text = "8px", font=("System", self.font_size), command=lambda: self.change_px_button_color(8), background=self.alt_background, borderwidth=0, activebackground=self.active_background, foreground=self.highlight, activeforeground=self.highlight)
         self.button_8px.grid(row=0, column=6)
 
-        self.button_16px = tk.Button(self.inputButtomFrame, text = "16px", font=("Arial", self.fontSize), command=lambda: self.change_px_button_color(16), background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+        self.button_16px = tk.Button(self.input_buttom_frame, text = "16px", font=("System", self.font_size), command=lambda: self.change_px_button_color(16), background=self.alt_background, borderwidth=0, activebackground=self.active_background, foreground=self.highlight, activeforeground=self.highlight)
         self.button_16px.grid(row=0, column=7)
 
-        self.button_32px = tk.Button(self.inputButtomFrame, text = "32px", font=("Arial", self.fontSize), command=lambda: self.change_px_button_color(32),background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+        self.button_32px = tk.Button(self.input_buttom_frame, text = "32px", font=("System", self.font_size), command=lambda: self.change_px_button_color(32),background=self.alt_background, borderwidth=0, activebackground=self.active_background, foreground=self.highlight, activeforeground=self.highlight)
         self.button_32px.grid(row=1, column=6)
 
-        self.button_64px = tk.Button(self.inputButtomFrame, text = "64px", font=("Arial", self.fontSize), command=lambda: self.change_px_button_color(64),background=self.altbackground, borderwidth=0, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
+        self.button_64px = tk.Button(self.input_buttom_frame, text = "64px", font=("System", self.font_size), command=lambda: self.change_px_button_color(64),background=self.alt_background, borderwidth=0, activebackground=self.active_background, foreground=self.highlight, activeforeground=self.highlight)
         self.button_64px.grid(row=1, column=7)
             
-        self.inputButtomFrame.pack()
+        self.input_buttom_frame.pack()
         self.change_number_button_color(0) #sets the 0 to be highlighted
         self.change_px_button_color(16) # sets 16 to be highlighted
 
-        self.numInDirectoryButton = tk.Label(self.root, text="Images in directory: " + str(self.image_count), font=("Arial", 10), foreground="White", background=self.background)
-        self.numInDirectoryButton.pack()
+        self.num_in_directory_button = tk.Label(self.root, text="Images in directory: " + str(self.image_count), font=("System", 10), foreground="White", background=self.background)
+        self.num_in_directory_button.pack()
 
-        self.drawingCanvas = tk.Canvas(self.root, width=256, height=256, bg=self.canvas_color, borderwidth = 0, highlightthickness= 0, relief="sunken")
-        self.drawingCanvas.pack()
+        self.drawing_canvas = tk.Canvas(self.root, width=256, height=256, bg=self.canvas_color, borderwidth = 0, highlightthickness= 0, relief="sunken")
+        self.drawing_canvas.pack()
 
-        self.trainModelButton = tk.Button(self.root, text= "Train Model", borderwidth=0, font=("Arial", self.fontSize), background=self.altbackground, activebackground=self.activebackground, foreground=self.highlight, activeforeground=self.highlight)
-        self.trainModelButton.pack(pady = 10)
+        # Bottom button frame w/ Train Model, Save Model, and Load Model
+        self.buttom_button_frame = tk.Frame(self.root)
+        for i in range (3):
+            self.buttom_button_frame.columnconfigure(i,weight =1)
 
-        self.toggleLightDarkButton = tk.Button(self.root, text = "Switch to Light Mode", command =self.changeColorPalette, background=self.altbackground, activebackground=self.activebackground, foreground=self.highlight, borderwidth=0, padx=5, pady=5)
-        self.toggleLightDarkButton.pack()
+        self.train_model_button = tk.Button(self.root, text= "Train Model", borderwidth=0, font=("System", self.font_size), background=self.alt_background, activebackground=self.active_background, foreground=self.highlight, activeforeground=self.highlight)
+        self.train_model_button.pack(pady = 10)
 
-        self.signatureLabel = tk.Label(self.root, text="Made by Aron Szucs", font=("Lucida Calligraphy", 10), borderwidth=0, background=self.background, foreground=self.highlight)
-        self.signatureLabel.pack(side="bottom")
+        self.toggle_light_dark_button = tk.Button(self.root, text = "Switch to Light Mode", command=self.change_color_palette, padx=5, pady=5, font=("System", 10), background=self.alt_background, activebackground=self.active_background, foreground=self.highlight, activeforeground=self.highlight, borderwidth=0)
+        self.toggle_light_dark_button.pack()
 
-    def canvasDraw(self, x, y):
-        if (x >= 0 and y >= 0 and x <= self.imageDimensions and y <= self.imageDimensions):
-            self.drawingCanvas.create_rectangle(x * (256 // self.imageDimensions), y * (256 // self.imageDimensions), (x * (256 // self.imageDimensions) + (256 // self.imageDimensions)), (y * (256 // self.imageDimensions) + (256 // self.imageDimensions)), fill = "white") 
+        self.signature_label = tk.Label(self.root, text="Made by Aron Szucs", font=("System", 10), borderwidth=0, background=self.background, foreground=self.color_highlight)
+        self.signature_label.pack(side="bottom")
+
+    def canvas_draw(self, x, y):
+        if (x >= 0 and y >= 0 and x <= self.image_dimensions and y <= self.image_dimensions):
+            self.drawing_canvas.create_rectangle(x * (256 // self.image_dimensions), y * (256 // self.image_dimensions), (x * (256 // self.image_dimensions) + (256 // self.image_dimensions)), (y * (256 // self.image_dimensions) + (256 // self.image_dimensions)), fill = "white") 
             self.array[y,x] = 1
 
     def change_number_button_color(self, num):
-        self.currentNum = num
+        self.current_num = num
 
         for button in self.buttons.values():
-            button.config(background=self.altbackground)
+            button.config(background=self.alt_background)
 
-        self.buttons[num].config(background = self.colorhighlight)
+        self.buttons[num].config(background = self.color_highlight)
 
     def change_px_button_color(self,num):
-        self.button_8px.config(background=self.altbackground)
-        self.button_16px.config(background=self.altbackground)
-        self.button_32px.config(background=self.altbackground)
-        self.button_64px.config(background=self.altbackground)
-        self.imageDimensions = num
-        self.array = numpy.zeros((self.imageDimensions, self.imageDimensions))
+        self.button_8px.config(background=self.alt_background)
+        self.button_16px.config(background=self.alt_background)
+        self.button_32px.config(background=self.alt_background)
+        self.button_64px.config(background=self.alt_background)
+        self.image_dimensions = num
+        self.array = numpy.zeros((self.image_dimensions, self.image_dimensions))
 
-        print(self.imageDimensions)
+        print(self.image_dimensions)
 
         if num == 8:
-            self.button_8px.config(background=self.colorhighlight)
+            self.button_8px.config(background=self.color_highlight)
         elif num == 16:
-            self.button_16px.config(background=self.colorhighlight)
+            self.button_16px.config(background=self.color_highlight)
         elif num == 32:
-            self.button_32px.config(background=self.colorhighlight)
+            self.button_32px.config(background=self.color_highlight)
         elif num == 64:
-            self.button_64px.config(background=self.colorhighlight)
+            self.button_64px.config(background=self.color_highlight)
 
-    def changeColorPalette(self):
+    def change_color_palette(self):
 
-        if self.LightOrDark == 0:
-            self.trainModelButton.configure(background=self.light_altbackground, highlightcolor=self.light_highlight)
+        if self.light_or_dark == 0:
+            self.train_model_button.configure(background=self.light_alt_background, highlightcolor=self.light_highlight)
             self.root.config(background=self.light_background)
             print("now light mode")
-            self.LightOrDark = 1
+            self.light_or_dark = 1
         else:
-            self.trainModelButton.configure(background=self.altbackground)
+            self.train_model_button.configure(background=self.alt_background)
             self.root.config(background=self.background)
             print("now dark mode")
-            self.LightOrDark = 0
+            self.light_or_dark = 0
 
-    def mouseDrag(self, event):
+    def mouse_drag(self, event):
         x, y = event.x, event.y
-        self.canvasDraw(x // (256 // self.imageDimensions) , y // (256 // self.imageDimensions))
-        print (str(x // (256 // self.imageDimensions)) + " " + str(y // (256 // self.imageDimensions)))
+        self.canvas_draw(x // (256 // self.image_dimensions) , y // (256 // self.image_dimensions))
+        print (str(x // (256 // self.image_dimensions)) + " " + str(y // (256 // self.image_dimensions)))
 
     def reset(self, event):
-        self.drawingCanvas.delete("all")
-        self.array = numpy.zeros((self.imageDimensions, self.imageDimensions))
+        self.drawing_canvas.delete("all")
+        self.array = numpy.zeros((self.image_dimensions, self.image_dimensions))
 
-    def saveImg(self, event):
+    def save_img(self, event):
         #draws image from array
         image = Image.fromarray((self.array * 255).astype(numpy.uint8))
 
-        file_path = "dataset/" + str(self.imageDimensions) + "px/" + str(self.currentNum) + "/num" + str(self.currentNum) + "count" + str(self.image_count + 1) + ".png"
+        file_path = "dataset/" + str(self.image_dimensions) + "px/" + str(self.current_num) + "/num" + str(self.current_num) + "count" + str(self.image_count + 1) + ".png"
 
         # makes dataset/ directory if not found
         if not os.path.isdir("dataset/"):
@@ -168,21 +172,21 @@ class AIDatasetCollector:
             print("made directory dataset/")
 
         # makes px directory if not found
-        if not os.path.isdir("dataset/" + str(self.imageDimensions) + "px/"):
-            os.mkdir("dataset/" + str(self.imageDimensions) + "px/")
-            print("made directory dataset/" + str(self.imageDimensions) + "px/")
+        if not os.path.isdir("dataset/" + str(self.image_dimensions) + "px/"):
+            os.mkdir("dataset/" + str(self.image_dimensions) + "px/")
+            print("made directory dataset/" + str(self.image_dimensions) + "px/")
 
         #makes imageDim directory if not found
-        if not os.path.isdir("dataset/" + str(self.imageDimensions) + "px/" + str(self.currentNum) + "/"):
-            os.mkdir("dataset/" + str(self.imageDimensions) + "px/" + str(self.currentNum) + "/")
-            print("made directory dataset/" + str(self.imageDimensions) + "px/" + str(self.currentNum) + "/")
+        if not os.path.isdir("dataset/" + str(self.image_dimensions) + "px/" + str(self.current_num) + "/"):
+            os.mkdir("dataset/" + str(self.image_dimensions) + "px/" + str(self.current_num) + "/")
+            print("made directory dataset/" + str(self.image_dimensions) + "px/" + str(self.current_num) + "/")
 
         image.save(file_path)
 
         self.reset(self)
 
-    def updateImagesInDirectory(self):
-        file_path = "dataset/" + str(self.imageDimensions) + "px/"+ str(self.currentNum) + "/"
+    def update_images_in_directory(self):
+        file_path = "dataset/" + str(self.image_dimensions) + "px/"+ str(self.current_num) + "/"
 
         # unable to find path
         if not os.path.isdir(file_path):
@@ -192,11 +196,11 @@ class AIDatasetCollector:
             # found on stack ovecanvasrflow
             self.image_count = len(os.listdir(file_path))
 
-        self.numInDirectoryButton.config(text="Images in directory: " + str(self.image_count))
+        self.num_in_directory_button.config(text="Images in directory: " + str(self.image_count))
 
-        self.root.after(100, self.updateImagesInDirectory)
+        self.root.after(100, self.update_images_in_directory)
 
-    def trainModel():
+    def train_model():
         print("TBD")
 
 # create main window
